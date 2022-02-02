82547a1d1d25e970aeec09c322ec4d3f5ee3dffd
priya2304 <haripriya10158@gmail.com>
2022-02-02 18:50:45 +0530

    .

---
+++ src/com/company/Comparisons.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Comparisons {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Integer integer1 = sc.nextInt();
+        Integer integer2 = 20;
+        System.out.println("compare output:" + integer1.compareTo(integer2));
+    }
+}
